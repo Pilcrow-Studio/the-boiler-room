18ec0497579f6ef2cc2adb0d9d401107c5a39a9d
Jonas Linnestad <jonas@humladigital.no>
2025-12-13 14:38:47 +0100

    Create robots.ts

---
+++ frontend/app/robots.ts
@@ -0,0 +1,16 @@
+import {MetadataRoute} from 'next'
+import {headers} from 'next/headers'
+
+export default async function robots(): Promise<MetadataRoute.Robots> {
+  const headersList = await headers()
+  const domain = headersList.get('host') as string
+
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: ['/api/', '/studio/'],
+    },
+    sitemap: `https://${domain}/sitemap.xml`,
+  }
+}
